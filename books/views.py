6d02c6f9529532b9951319c05148e5c793d3af2c
Oneday2010 <jozxz1691@gmail.com>
2021-12-27 21:58:16 +0000

    add search and filter

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Book, Category
 
 # Create your views here.
 
@@ -7,9 +9,28 @@ def all_books(request):
     """ A view to show all books, including sorting and search queries """
 
     books = Book.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            books = books.filter(category__title__in=categories)
+            categories = Category.objects.filter(title__in=categories)
+            
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('books'))
+            
+            queries = Q(title__icontains=query) | Q(author__icontains=query)
+            books = books.filter(queries)
 
     context = {
         'books': books,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'books/books.html', context)
