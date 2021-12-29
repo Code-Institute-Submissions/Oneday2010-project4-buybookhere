a9c30bafd80a5518fb4474f358ff116e2334459c
Oneday2010 <jozxz1691@gmail.com>
2021-12-29 20:44:21 +0000

    add contacts.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,7 +5,7 @@ def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    book_count = 0
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -19,7 +19,7 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'book_count': book_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
