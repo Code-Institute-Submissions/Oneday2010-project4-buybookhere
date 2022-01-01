ee725e08ca0b808b922fe386e90947e99e1a2fca
Oneday2010 <jozxz1691@gmail.com>
2022-01-01 12:32:39 +0000

    create js and update static on check out with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K81yOIeaCsGdWpZnwy9DcLfLaLHWYdD8xg7lrXjAGU88j7ibMKaI6yxiZcpjDBsumNg0rWZ1hzJV6CWcJuqETxl00oTrsCGqS',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
