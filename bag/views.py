253f78eb59bc263f3d892434974f4fb8cbda62f7
Oneday2010 <jozxz1691@gmail.com>
2022-01-07 21:12:27 +0000

    fixed loading error

--- bag/views.py
+++ bag/views.py
@@ -1,20 +1,17 @@
-from django.shortcuts import render, redirect, reverse
-, HttpResponse, get_object_or_404
-
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
+
 from books.models import Book
 # Create your views here.
-
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified book to the shopping bag """
-
+    
     book = get_object_or_404(Book, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -29,7 +26,6 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified book to the specified amount"""
