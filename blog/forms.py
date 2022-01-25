0b5a2dc18da39ef018bb655a38bb8027f6c984ae
Oneday2010 <jozxz1691@gmail.com>
2022-01-25 21:12:39 +0000

    add_blog and edit_blog.html

---
+++ blog/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .widgets import CustomClearableFileInput
+from .models import Blog
+
+
+class BlogForm(forms.ModelForm):
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False,
+                             widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'intro': 'Introduction',
+            'body_one': 'Paragraph 1',
+            'body_two': 'Paragraph 2',
+            'image': 'Image',
+            'image_url': 'Image Url',
+            'author': 'Author',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'input-shadow \
+                border-black rounded-0'
+            self.fields[field].label = False
